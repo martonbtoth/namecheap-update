ea9b90e59c68d0cb2fb48b5f58a939e037971be6
Marton Toth <toth.marton33@gmail.com>
2014-04-06 13:58:51 +0000

    Woot

--- namecheap.py
+++ namecheap.py
@@ -1,6 +1,19 @@
 import requests
+import configparser 
 
 if __name__ == '__main__':
-    print('Namecheap updater v0.1');
+    print('-----Namecheap updater v0.1----');
+    config = configparser.ConfigParser()
+    config.read('namecheap.conf')
+    host = config['namecheap']['host']
+    domain = config['namecheap']['domain']
+    passwd = config['namecheap']['password']
+    print('Host: ' + host)
+    print('Domain: ' + domain)
     r = requests.get('http://wtfismyip.com/text')
+    ip = r.text
+    print('IP: ' + ip)
+    url = 'https://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s' % (host, domain, passwd, ip)
+    print('\nUpdating host record...')
+    r = requests.get(url)
     print(r.text)
