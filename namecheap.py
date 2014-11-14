9322d0f16f41eb87ff5398dedf1106a32e52d86a
Márton Tóth <toth.marton33@gmail.com>
2014-11-14 19:18:21 +0100

    Will only update when ip changes.

--- namecheap.py
+++ namecheap.py
@@ -1,6 +1,22 @@
 import requests
 import configparser 
 
+def checkip (ip):
+    try:
+        f = open('/tmp/previp.txt', 'r')
+        previp = f.readline()
+        f.close()
+        if previp == ip:
+            return True
+        return False
+    except FileNotFoundError:
+        return False
+
+def writeip (ip):
+    f = open ('/tmp/previp.txt', 'w')
+    f.write(ip)
+    f.close
+
 if __name__ == '__main__':
     print('-----Namecheap updater v0.1----');
     config = configparser.ConfigParser()
@@ -13,7 +29,11 @@ if __name__ == '__main__':
     r = requests.get('http://wtfismyip.com/text')
     ip = r.text
     print('IP: ' + ip)
-    url = 'https://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s' % (host, domain, passwd, ip)
-    print('\nUpdating host record...')
-    r = requests.get(url)
-    print(r.text)
+    if checkip(ip) == False:
+        writeip(ip)
+        url = 'https://dynamicdns.park-your-domain.com/update?host=%s&domain=%s&password=%s&ip=%s' % (host, domain, passwd, ip)
+        print('\nUpdating host record...')
+        r = requests.get(url)
+        print(r.text)
+    else:
+        print('IP matches previous address, skipping update.')
